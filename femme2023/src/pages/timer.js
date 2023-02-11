384c33cbe1bcd534f7309f091e07fc3497cc8f7b
Skylar Rearick <rearick.skylar@gmail.com>
2023-02-11 17:34:46 -0500

    added dummy pom timer

--- femme2023/src/pages/timer.js
+++ femme2023/src/pages/timer.js
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import "./timer.css";
 
-const Timer = () => {
-  return <h1>You can see the timer!</h1>;
-};
+function Timer() {
+  const [secondsLeft, setSecondsLeft] = useState(25 * 60);
+  const [timer, setTimer] = useState();
+
+  const start = () => {
+    const timer = setInterval(() => {
+      setSecondsLeft((secondsLeft) => secondsLeft - 1);
+      if (secondsLeft === 0) {
+        clearInterval(timer);
+      }
+    }, 1000);
+    setTimer(timer);
+  };
+
+  useEffect(() => {
+    if (secondsLeft === 0) {
+      clearInterval(timer);
+    }
+  }, [secondsLeft, timer]);
+  useEffect(() => {
+    return () => clearInterval(timer);
+  }, [timer]);
+
+  return (
+    <div className="pomodoro">
+      <h1>Pomodoro Timer</h1>
+      <button onClick={start}>start</button>
+      <div>{secondsLeft} seconds left</div>
+    </div>
+  );
+}
 
 export default Timer;
